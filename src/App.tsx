6322b666ba0ede02a148d51634901fda42df7490
ivos.pleva <ivos.pleva@gmail.com>
2025-04-11 11:16:54 +0000

    feat: Clean up Vite React TS template

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold">ProcessMapper v2 (React Frontend)</h1>
+      {/* Zde bude hlavní router a obsah aplikace */}
+    </div>
+  )
+}
+
+export default App
